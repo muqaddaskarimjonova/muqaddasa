ff5f6d3c178dd45e184e60d734dcf6933045ee78
muqaddaskarimjonova <124684968+muqaddaskarimjonova@users.noreply.github.com>
2023-02-09 16:23:24 +0500

    cod
    hafta kunlari

---
+++ hafta kunlarining nomini hisoblaw .cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+​
+int main() {
+  int day = 4;
+  switch (day) {
+  case 1:
+    cout << "Dushanba";
+    break;
+  case 2:
+    cout << "Seshanba";
+    break;
+  case 3:
+    cout << "Chorshanba";
+    break;
+  case 4:
+    cout << "Payshanba";
+    break;
+  case 5:
+    cout << "Juma";
+    break;
+  case 6:
+    cout << "Shanba";
+   break;
+  case 7:
+    cout << "Yakshanba";
+    break;
+  }
+  return 0;
+}
