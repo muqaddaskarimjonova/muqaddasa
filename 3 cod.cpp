8863c00d4f781d8cf000bf75f909f3038623d5f8
muqaddaskarimjonova <124684968+muqaddaskarimjonova@users.noreply.github.com>
2023-02-09 16:27:25 +0500

    cod
    double mul

---
+++ 3 cod.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+double mul(double x,double y);
+int main()
+{
+  double answer;
+    answer=mul(677,46677);
+    cout<<"javobi ravno bilan."<<answer;
+    
+    return 0;
+}
+double  mul(double x,double y)
+{
+return x/y;
+}
