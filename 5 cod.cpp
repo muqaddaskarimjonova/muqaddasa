964e1f6a61351e63dc9b23b9f79f9ae8449fca0d
muqaddaskarimjonova <124684968+muqaddaskarimjonova@users.noreply.github.com>
2023-02-09 16:28:14 +0500

    cod
    bita code

---
+++ 5 cod.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    system("color 0C");
+    int count;
+    for(count=0; count<= 2021; count=count+1)
+        cout << "god Nash";
+        cout << count << " ";
+    return 0;
+}
