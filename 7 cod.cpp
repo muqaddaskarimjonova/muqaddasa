8482c0d2023fa446a3133f07380b3212e33ed67e
muqaddaskarimjonova <124684968+muqaddaskarimjonova@users.noreply.github.com>
2023-02-09 16:32:21 +0500

    cod
    cod

---
+++ 7 cod.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+  int a, b;
+  cout << " 4: "; cin >> a;
+  cout << " 8: "; cin >> b;
+  if
+  (a < b) {
+      cout << " 5./n";
+      cout << " 12: " << b-a;
+  }
+
+     return 0;
+}
+
