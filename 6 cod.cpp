1195195ad7810db9e19f083182f5787aa73604a6
muqaddaskarimjonova <124684968+muqaddaskarimjonova@users.noreply.github.com>
2023-02-09 16:28:47 +0500

    cod
    cod

---
+++ 6 cod.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+ int main()
+ {
+     int a,b;
+     cout <<"vedite pervoe cislo: "; cin >>a;
+     cout <<"vedite vtoroe cislo: "; cin >>b;
+     if(a < b) {
+    cout << "pervoe cislo menwe vtorogo.\n";
+    cout << "ix raznost ravna: " << b-a;
+     }
+     return 0;
+ }
+
