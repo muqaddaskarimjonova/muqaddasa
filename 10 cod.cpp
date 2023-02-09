81dc760acc37cdda986200d6b8e596d87f460771
muqaddaskarimjonova <124684968+muqaddaskarimjonova@users.noreply.github.com>
2023-02-09 16:34:20 +0500

    cod
    cod

---
+++ 10 cod.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double dollar, sum;
+    cout << "Nechi dollardi pulga o'tkazmoqchisiz: ";
+    cin >> dollar;
+    sum = dollar * 11300;
+    cout << "sum: " << sum; cout << " Bo'ladi";
+
+    return 0;
+}
